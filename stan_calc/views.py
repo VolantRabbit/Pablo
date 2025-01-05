3e59a4907e018d65f15d8fc2db662cc5cb7864b7
Billy <billytrimkz@gmail.com>
2025-01-05 21:43:47 +0100

    currency to template

--- stan_calc/views.py
+++ stan_calc/views.py
@@ -1,7 +1,17 @@
+import requests
 from django.shortcuts import render
-from .forms import SumForm  # Import the form
 
-
+def get_exchange_rates():
+    currency_code = "eur"
+    url = f"http://www.floatrates.com/daily/{currency_code}.json"
+    response = requests.get(url)
+    data = response.json()
+    eur_rate = data.get("rsd", {}).get("rate", None)
+    return eur_rate
 def calc(request):
+    eur_rate = None
+    if request.method == 'POST':
+        eur_rate = get_exchange_rates()  # Get the EUR rate on button click
+    return render(request, 'stan_calc/calc.html', {'eur_rate': eur_rate})
 
-    return render(request, 'stan_calc/calc.html', )
+
